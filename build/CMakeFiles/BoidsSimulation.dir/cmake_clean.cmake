38a86b8ba09c3e35a774dc7d2eaf2524df969c72
Vinícius <vinicius@email.com>
2025-11-12 12:56:58 -0300

    add wsl build files and executable

---
+++ build/CMakeFiles/BoidsSimulation.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "BoidsSimulation"
+  "BoidsSimulation.pdb"
+  "CMakeFiles/BoidsSimulation.dir/external/glad/src/glad.c.o"
+  "CMakeFiles/BoidsSimulation.dir/external/glad/src/glad.c.o.d"
+  "CMakeFiles/BoidsSimulation.dir/src/Boid.cpp.o"
+  "CMakeFiles/BoidsSimulation.dir/src/Boid.cpp.o.d"
+  "CMakeFiles/BoidsSimulation.dir/src/Camera.cpp.o"
+  "CMakeFiles/BoidsSimulation.dir/src/Camera.cpp.o.d"
+  "CMakeFiles/BoidsSimulation.dir/src/Flock.cpp.o"
+  "CMakeFiles/BoidsSimulation.dir/src/Flock.cpp.o.d"
+  "CMakeFiles/BoidsSimulation.dir/src/Shader.cpp.o"
+  "CMakeFiles/BoidsSimulation.dir/src/Shader.cpp.o.d"
+  "CMakeFiles/BoidsSimulation.dir/src/main.cpp.o"
+  "CMakeFiles/BoidsSimulation.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/BoidsSimulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
